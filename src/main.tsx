04647661f4ddc0aa365dc410424b988ce720df8d
Yusuke <happy0212322@gmail.com>
2024-05-14 00:44:33 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { App } from './App.tsx'
-// import './index.css'
 import Home from "./Home.tsx";
 import App from "./App.tsx";
 import Test from "./components/Test.tsx";
